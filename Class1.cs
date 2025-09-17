4a6da3b5098e29d807d11efd820d4a4be86bf816
zhangzhangzf <641522359@qq.com>
2025-09-18 00:07:25 +0800

    Initial commit

--- Class1.cs
+++ Class1.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Depot_Design_Toolbox
+using System.Data;
+using System.Diagnostics;
+using ZwSoft.ZwCAD.ApplicationServices;
+using ZwSoft.ZwCAD.Runtime;
+using ZwSoft.ZwCAD.DatabaseServices;
+using ZwSoft.ZwCAD.Geometry;
+namespace TEST
 {
-    public class Class1
+    public class SHMain
     {
+        [CommandMethod("AddCircle")]
+        public void AddCircle()
+        {
+            Document zcDoc = Application.DocumentManager.MdiActiveDocument;
+            Database zcDB = zcDoc.Database;
+            Transaction ZcTran = zcDoc.TransactionManager.StartTransaction();
+            using (ZcTran)
+            {
+                BlockTable zcBLT = (BlockTable)ZcTran.GetObject(zcDB.BlockTableId, OpenMode.ForRead);
+                BlockTableRecord zcBLTR = (BlockTableRecord)ZcTran.GetObject(zcBLT[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+                Circle zcCircle = new Circle();
+                zcCircle.Center = new Point3d(2, 3, 0);
+                zcCircle.Radius = 30;
+                zcCircle.ColorIndex = 1;
+                zcBLTR.AppendEntity(zcCircle);
+                ZcTran.AddNewlyCreatedDBObject(zcCircle, true);
+                ZcTran.Commit();
+            }
+            zcDoc.SendStringToExecute("_ZOOM E ", false, false, false);
+        }
     }
 }
